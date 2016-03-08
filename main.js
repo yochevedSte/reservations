3b7134caea131d0409659486ba4f5008dc10ecd0
amhayslip <amhayslip@gmail.com>
2016-03-08 12:33:53 +0200

    fix it

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var reservations = {
   'Bob': { claimed: false },
+  'Ted': { claimed: true }
 }
 
 var name = prompt('Please enter the name for your reservation');
 
 var claimReservation = function () {
-  // write your code here
+  // write your code here!
 }
 
-addReservation();
+claimReservation();

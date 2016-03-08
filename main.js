7aa5f93bdce783747a1a037e3bb2337c84fd594a
amhayslip <amhayslip@gmail.com>
2016-03-08 12:01:56 +0200

    add

---
+++ main.js
@@ -0,0 +1,11 @@
+var reservations = {
+  'Aaron': { claimed: false },
+}
+
+var name = prompt('Please enter the name for your reservation');
+
+var addReservation = function () {
+  // write your code here
+}
+
+addReservation();

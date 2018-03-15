3da569602d611457cb428abe95cb770b8c7ba86f
Yocheved <cheved.stern@gmail.com>
2018-03-15 13:43:48 +0200

    completed required tasks

--- main.js
+++ main.js
@@ -3,10 +3,53 @@ var reservations = {
   'Ted': { claimed: true }
 }
 
-var name = prompt('Please enter the name for your reservation');
+var table = document.getElementById("reservationTable");
 
-var claimReservation = function () {
-  // write your code here!
+
+var addReservation = function(name)
+{
+   var row = table.insertRow();
+  var nameCell = row.insertCell(0);
+  var claimedCell = row.insertCell(1);
+  nameCell.innerHTML = name;
+  if(reservations[name].claimed)
+   claimedCell.innerHTML = "yes";
+  else 
+   claimedCell.innerHTML = "no";
 }
 
-claimReservation();
+var claimReservation = function () {
+  var name = document.getElementById("name").value;
+  if(name === ""){
+    alert("Please enter your name for reservation");
+    return;
+  }
+    
+  name = (name.toLowerCase());
+  name = capitalizeFirstLetter(name);
+  if(reservations[name]){
+    if(reservations[name].claimed === true )
+      alert("Welcome " + name + " to our restaurant!");
+    else
+      alert ("The reservation under the name " + name + " has been claimed");
+  }
+  else{
+    alert(name + ", we will make you a reservation. ");
+    reservations[name]= {claimed: true};
+    alert("Your reservation was made under the name " + name);
+    addReservation(name);
+  
+  
+  }
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+function loadTable(){
+  for(var name in reservations){
+    addReservation(name);
+  }
+}
